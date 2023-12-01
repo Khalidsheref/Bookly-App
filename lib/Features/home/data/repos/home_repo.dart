72747106970bcca6071483966b4477e63f28c6e6
khalidsheref <khalidsheref@gmail.com>
2023-12-01 15:12:26 +0200

    create book model

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchNewsetBooks();
-  fetchFeaturedBooks();
-  fetchSimilarBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchSimilarBooks();
 }
